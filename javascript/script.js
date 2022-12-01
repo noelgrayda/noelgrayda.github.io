5b8e9cd61850b997893ed8170aa1cd7c7ace647e
Noel Grayda <noelgrayda@gmail.com>
2022-12-01 21:01:53 +0800

    First commit

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+
+function buttonAccept() {
+    document.querySelector("#acceptCookies").remove();
+}
+
+function locationMap() {    
+    alert("Loading weather report");
+}
+
+function temperature() {   
+    
+        document.querySelector("#red1").innerHTML = 75 + "°";
+        document.querySelector("#red2").innerHTML = 80 + "°";
+        document.querySelector("#red3").innerHTML = 69 + "°";
+        document.querySelector("#red4").innerHTML = 78 + "°";
+
+        document.querySelector("#blue1").innerHTML = 65 + "°";
+        document.querySelector("#blue2").innerHTML = 66 + "°";
+        document.querySelector("#blue3").innerHTML = 61 + "°";
+        document.querySelector("#blue4").innerHTML = 70 + "°";
+}
